8bccde22dc3a86449a61b42928c464d18d271367
Joakim Thorum Poulsen <Joakim_poulzen@live.dk>
2016-11-15 16:12:06 +0100

    Update DiceCup.java

--- src/diceGame/DiceCup.java
+++ src/diceGame/DiceCup.java
@@ -1,5 +1,36 @@
 package diceGame;
 
 public class DiceCup {
+	private int[] values;
+	private int sides;
+	
+	public DiceCup(int diceSides, int diceAmount){
+		values = new int[diceAmount];
+		this.sides = diceSides;	
+		this.setAllValuesRandom();
+	}
+	
+	public int[] getValues(){
+		return values;
+	}
+	
+	
+	public void setAllValues(int[] values){
+		this.values = values;
+	}
 
+	public int getSum(){
+		int sum = 0;
+		for (int i = 0; i<values.length;i++){
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+	public void setAllValuesRandom(){
+		for (int i = 0; i < values.length; i++){
+			values[i] = ( (int) (Math.random()*sides)+1);
+		}
+	}
+}
 }
