840ec69414b821713f4d382c5eff4ca1b673a278
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-15 15:02:09 +0100

    Changed rent datatype to final

--- src/diceGame/Fleet.java
+++ src/diceGame/Fleet.java
@@ -6,10 +6,10 @@
 package diceGame;
 
 public class Fleet extends Ownable {
-	private int rent_1 = 500;
-	private int rent_2 = 1000;
-	private int rent_3 = 2000;
-	private int rent_4 = 4000;
+	private final int rent_1 = 500;
+	private final int rent_2 = 1000;
+	private final int rent_3 = 2000;
+	private final int rent_4 = 4000;
 	
 	public Fleet(int price, Player owner) {
 		super(price, owner);
