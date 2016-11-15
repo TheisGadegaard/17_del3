dacfb5dda4f6eb143c914b8685736f7d946fef9b
Freya Helstrup <freyagh@me.com>
2016-11-15 16:59:55 +0100

    Updated CreateGame, Game and StartProgram

--- src/diceGame/Game.java
+++ src/diceGame/Game.java
@@ -1,5 +1,26 @@
 package diceGame;
 
 public class Game {
-
+	private Board board; //An instance of the Board class
+	private Player[] players; //An array of Players
+	private DiceCup dice; //An instance of the DiceCup class
+	private final int diceAmount; //The amount of dice used by the game
+	private final int diceSides; //The number of sides the dice can have
+	
+	public Game(){
+		diceAmount = 2;		diceSides = 6;
+		dice = new DiceCup(diceSides,diceAmount);
+		board = new Board();
+	}
+	
+	public void resetGame(int playerAmount){
+		players = new Player[playerAmount];
+		for (int i = 0; i < players.length; i++){
+			players[i] = new Player(i+1,0);
+		}
+	}
+	
+	public void playGame(){
+		
+	}
 }
