dbe026565a328f1470877a8a036e91cb1f76d95e
Freya Helstrup <freyagh@me.com>
2016-11-15 14:47:45 +0100

    Added variables and methods to Ownable class.
    - There's still work to do on getRent method.

--- src/diceGame/Ownable.java
+++ src/diceGame/Ownable.java
@@ -1,5 +1,16 @@
 package diceGame;
 
-public class Ownable extends Field {
+public abstract class Ownable extends Field {
+	protected int price;
+	protected Player owner;
+
+	public Ownable(int price, Player owner){
+		this.price = price;
+		this.owner = owner;
+	}
+
+	public int getRent(){
+		return 0;
+	}
 
 }
