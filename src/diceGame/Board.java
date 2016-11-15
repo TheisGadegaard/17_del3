f9c8db94425a0ab19233b1409e46d277e010cac3
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-15 15:37:46 +0100

    Small change in order of comments and code

--- src/diceGame/Board.java
+++ src/diceGame/Board.java
@@ -5,9 +5,9 @@ public class Board {
 	
 	public Board(){
 		//We can instantiate an object in the field array by using a subclass' constructor
-		fields[0] = new Refuge();
 		//The refuge will get typecasted into a field, but we can downcast it and get a refuge again
 		//Remember to use the appropriate constructors
+		fields[0] = new Refuge();
 		
 	}
 }
