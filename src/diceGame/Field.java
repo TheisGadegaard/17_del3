b17b8e39c8989b215bd026415da314b0d7fefc24
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-15 14:55:24 +0100

    Added base structure of LandOnField
    The base structure for downcasting is now in place, but addtional
    development of the subclasses is necessary before more work can be done.

--- src/diceGame/Field.java
+++ src/diceGame/Field.java
@@ -1,5 +1,34 @@
 package diceGame;
 
 public class Field {
+	
+	public void LandOnField(Field field){
+		
+		// We use instanceof operator to check if field has a subclass
+		if (field instanceof Ownable){
+			
+			//If field has the ownable subclass, check which ownable field type it is
+			if(field instanceof Territory){
+				//Reference use case "land on territory"
+				
+			}
+			else if(field instanceof LaborCamp){
+				//Reference use case "land on labor camp"
+				
+			}
+			else if(field instanceof Fleet){
+				//Reference use case "land on fleet"
+				
+			}
+		}
+		else if (field instanceof Tax){
+			 //Reference use case "land on tax"
+			
+		}
+		else if (field instanceof Refuge){
+			//Reference use case "land on refuge"
+			
+		}
+	}
 
 }
