eadbc4554035551a19c54755ef1da4d14d1c6f70
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-15 15:31:37 +0100

    Added basic instantiation idea

--- src/diceGame/Board.java
+++ src/diceGame/Board.java
@@ -1,5 +1,12 @@
 package diceGame;
 
 public class Board {
-
+	Field[] fields = new Field[21];
+	
+	public Board(){
+		//We can instantiate an object in the field array by using a subclass' constructor
+		fields[0] = new Refuge();
+		//The refuge will get typecasted into a field, but we can downcast it and get a refuge again
+		//Remember to use the appropriate constructors
+	}
 }
