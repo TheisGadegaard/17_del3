6e26f61cff5f1a335a9a9da96bc798b17dbe98ee
Danny Jønsson <dannyjoensson@gmail.com>
2016-11-15 15:51:00 +0100

    Finished class Piece
    Added attributes and methods that was designed in the analyse class diagram.

--- src/diceGame/Piece.java
+++ src/diceGame/Piece.java
@@ -1,5 +1,25 @@
+/* This class has the responsibility of handling the player's piece/car.
+ * It carries two private variables:
+ * 		- An integer with the current board-position of the piece
+ * 		- A color for the piece's color.
+ */
+
 package diceGame;
+import java.awt.Color;
+
 
 public class Piece {
+	private int position;
+	private Color color;
 
+	// Constructor - In order to create the piece, you will need to give the vehicle a color using java.awt.Color;
+	public Piece(Color color) {
+		this.color = color;
+	}
+
+	// Move the piece to a position using an integer
+	public void movePieceTo(int position) {
+		this.position = position;
+	}
 }
+
