dda59a9eb0b7025e8a067c2a8bfbd5da2e8b44a3
Freya Helstrup <freyagh@me.com>
2016-11-15 16:14:59 +0100

    Created Tax class

--- src/diceGame/Tax.java
+++ src/diceGame/Tax.java
@@ -1,5 +1,23 @@
+/**
+ * @version 1.0
+ * @author freya
+ */
 package diceGame;
 
 public class Tax extends Field {
+	private int taxAmount;
+	private int taxRate; //taxRate = 0 means no taxRate.
+	
+	public Tax(int taxAmount, int taxRate){
+		this.taxAmount = taxAmount;
+		this.taxRate = taxRate;
+	}
+	
+	public int getTaxAmount(){
+		return taxAmount;
+	}
 
+	public int getTaxRate(){
+		return taxRate;
+	}
 }
