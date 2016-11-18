147bb86aa48530cc8956fda30acbbc7d53ceb648
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-18 09:04:48 +0100

    Added name as attribute

--- src/diceGame/Field.java
+++ src/diceGame/Field.java
@@ -6,8 +6,11 @@
 package diceGame;
 
 public abstract class Field {
+	private String name;
 	
 	public abstract void landOnField(Player player);
 
-
+	public void setName(String text){
+		name = text;
+	}
 }
