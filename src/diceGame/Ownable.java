cd04b592524fcadbc6947d3acc3f69f0d8deee9c
Freya Helstrup <freyagh@me.com>
2016-11-18 11:45:28 +0100

    Ownable: landOnField method updated

--- src/diceGame/Ownable.java
+++ src/diceGame/Ownable.java
@@ -1,5 +1,5 @@
 /**
- * @version 1.1
+ * @version 1.2
  * @author freya, tobias
  */
 
@@ -8,13 +8,14 @@ package diceGame;
 public abstract class Ownable extends Field {
 	protected int price;
 	protected Player owner;
-
+ 
 	public Ownable(int price){
 		this.price = price;
+		this.owner = null;
 	}
 
 	public abstract int getRent();
-	
+
 	public Player getOwner(){
 		return owner;
 	}
@@ -26,8 +27,29 @@ public abstract class Ownable extends Field {
 	public void setOwner(Player owner){
 		this.owner = owner;
 	}
-	
+
 	public void landOnField(Player player){
+		if (owner == null){
+			//GUI choice to buy field if it is not owned
+			//owner = player;
+		}
+		else if (owner == player){
+			//nothing should happen if field is owned by player
+		}
+		else{//pay rent to owner if owned
+			if (this instanceof LaborCamp){
+				//when LaborCamp we should multiply dice sum with 100 and number of owned labor camps
+				
+				//int rent = getRent()*player.getDiceSum();
 		
+//				owner.setBalance(player.getBalance() + rent;
+//				player.setBalance(player.getBalance() - rent;
+			}
+			else{
+				owner.setBalance(player.getBalance() + getRent());
+				player.setBalance(player.getBalance() - getRent());
+			}
+		}
+
 	}
 }
