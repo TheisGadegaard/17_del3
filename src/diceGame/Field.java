62d065c1e4056adaea350f38893a54d97737660a
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-21 13:07:52 +0100

    Organizing the code
    -Added getName()
    -Removed warning on Name (see above)
    -Commented abstract method

--- src/diceGame/Field.java
+++ src/diceGame/Field.java
@@ -8,9 +8,20 @@ package diceGame;
 public abstract class Field {
 	private String name;
 	
+	/*
+	 * Field is an abstract class. 
+	 * Having an abstract method means that every class that 
+	 * extends it will also have that method.
+	 * We can then do different implementations of this method
+	 * for each of our specific classes.
+	 */
 	public abstract void landOnField(Player player);
 
 	public void setName(String text){
 		name = text;
+	}
+	
+	public String getName(){
+		return name;
 	}
 }
