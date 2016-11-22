a27e494b16b51add08ed40fec70fabc5c13deef9
Gustav Petersen <Rangewaver@gmail.com>
2016-11-22 14:59:29 +0100

    Added comments

--- src/diceGame/DiceCup.java
+++ src/diceGame/DiceCup.java
@@ -4,6 +4,8 @@ public class DiceCup {
 	private int[] values;
 	private int sides;
 	
+//Constructor to set amount of dice-sides and the amount of dices
+//furthermore uses the setAllValuesRandom method which simulates a roll
 	public DiceCup(int diceSides, int diceAmount){
 		values = new int[diceAmount];
 		this.sides = diceSides;	
@@ -30,6 +32,7 @@ public class DiceCup {
 		return sum;
 	}
 	
+	//Simulates a roll of the chosen dice(s)
 	public void setAllValuesRandom(){
 		for (int i = 0; i < values.length; i++){
 			values[i] = ( (int) (Math.random()*sides)+1);
