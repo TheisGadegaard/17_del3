ed085e2fc54f2a1ad9124b6c08acc930a8026ea3
Gustav Petersen <Rangewaver@gmail.com>
2016-11-22 15:31:24 +0100

    Copied previous Unit test

--- src/diceGameTest/DiceCupTest.java
+++ src/diceGameTest/DiceCupTest.java
@@ -1,5 +1,38 @@
+
 package diceGameTest;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import diceGame.DiceCup;
+
 public class DiceCupTest {
+
+	@Test
+	public void test() {
+
+		int[] Runs = new int[30000];
+
+		Boolean FailOnce = false;
+		int DiceSides = 6;
+
+		DiceCup Cup = new DiceCup(DiceSides,1);
+
+		for (int i = 0; i < 30000; i++){
+
+			Cup.setAllValuesRandom();
+			Runs[i] = Cup.getSum();
+			
+			if (Runs[i] <= 0 || Runs[i] > DiceSides ){
+				
+				FailOnce = true;
+			
+			}
+		}
+
+		assertEquals(FailOnce, false);
+
+	}
 
 }
