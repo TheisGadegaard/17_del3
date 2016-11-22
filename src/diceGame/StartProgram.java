a4ec9300e8b202d645f10e3dc747b0be5fe60247
Gustav Petersen <Rangewaver@gmail.com>
2016-11-22 15:02:17 +0100

    Added comments

--- src/diceGame/StartProgram.java
+++ src/diceGame/StartProgram.java
@@ -4,6 +4,9 @@ public class StartProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Instantiates the CreateGame method which is the only 
+		//responsibility for this class
 		new CreateGame();
 		
 	}
