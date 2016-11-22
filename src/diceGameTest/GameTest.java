3c1278bc641dc06da68c042833320d6602935688
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-22 16:06:52 +0100

    Made class for testing game
    -added GameTest
    -changed .classpath

---
+++ src/diceGameTest/GameTest.java
@@ -0,0 +1,16 @@
+package diceGameTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import diceGame.Game;
+
+public class GameTest extends Game {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
