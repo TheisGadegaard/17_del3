3170f420cdedfc8c5c92bea7fd218397ed39be35
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-23 10:47:23 +0100

    Added field test class for use case and unit testing

---
+++ src/diceGameTest/FieldTest.java
@@ -0,0 +1,43 @@
+package diceGameTest;
+
+import static org.junit.Assert.*;
+import diceGame.*;
+
+import org.junit.Test;
+
+public class FieldTest {
+	/*
+	 * Unit tests of the class Field and each of the classes in its inheritance hierarchy
+	 */
+	@Test
+	public void fleetTest() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void territoryTest() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void refugeTest() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void laborCampTest() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void taxTest() {
+		
+		fail("Not yet implemented");
+	}
+	
+
+}
