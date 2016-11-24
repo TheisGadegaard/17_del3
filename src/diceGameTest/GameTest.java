f9ec3de5e9bbc3ef9a35c55119bcb3953ee4e1e7
Freya Helstrup <freyagh@me.com>
2016-11-24 22:12:44 +0100

    Added Test case for Use Case 1.

--- src/diceGameTest/GameTest.java
+++ src/diceGameTest/GameTest.java
@@ -1,15 +1,35 @@
 package diceGameTest;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import diceGame.Player;
 
 public class GameTest {
 
+	/**
+	 * UC01: Land on Fleet
+	 */
 	@Test
-	public void test() {
+	public void TC01() {
+		int startBalance = 5000;
+		
+	//First case we let the player buy the field
 		GameTestMode game = new GameTestMode();
-		fail("Not yet implemented");
+		
+		DiceCupTestMode dice = new DiceCupTestMode(2,6);
+		dice.setValues(new int[]{2,2});
+		Player nextPlayer = game.playTurn(game.getPlayers()[0], dice);
+
+		assertEquals(nextPlayer, game.getPlayers()[1]);
+		assertEquals(game.getPlayers()[0].getBalance(), startBalance-4000);
+		
+	//Second case we let the second player pay rent to owner
+		dice.setValues(new int[]{2,2});
+		nextPlayer = game.playTurn(game.getPlayers()[1], dice);
+
+		assertEquals(nextPlayer, game.getPlayers()[0]);
+		assertEquals(game.getPlayers()[1].getBalance(), startBalance-500);
 	}
 
 }
--- src/diceGameTest/GameTestMode.java
+++ src/diceGameTest/GameTestMode.java
@@ -1,15 +1,43 @@
 package diceGameTest;
 
-import diceGame.Game;
+import desktop_resources.GUI;
+import diceGame.*;
 
 public class GameTestMode extends Game {
-
+	DiceCupTestMode dice;
+	
 	public GameTestMode(){
 		super();
-		super.resetGame(3, 5000);
+		super.resetGame(2, 5000);
 	}
 	
 	public int getPlayerArrayLength(){
 		return super.players.length;
 	}
+	
+	public Player[] getPlayers(){
+		return players;
+	}
+	
+	public Player playTurn(Player currentPlayer, DiceCupTestMode dice){
+//		GUI.getUserButtonPressed(Messages.getGMessages()[11] + currentPlayer.getName() + Messages.getGMessages()[12], Messages.getGMessages()[7]);
+		
+		throwDice(currentPlayer, dice);
+		super.dice = this.dice;
+
+		movePiece(currentPlayer);
+
+		currentField.landOnField(currentPlayer);
+
+		Player nextPlayer = defineNextPlayer(currentPlayer);
+		return nextPlayer;
+	}
+	
+	protected void throwDice(Player currentPlayer, DiceCupTestMode dice){
+		this.dice = dice;
+		
+		currentPlayer.setDiceSum(dice.getSum());
+		GUI.setDice(dice.getValues()[0], dice.getValues()[1]);
+	}
+
 }
